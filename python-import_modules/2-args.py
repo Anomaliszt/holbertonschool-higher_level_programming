67dfb9a792fce1eef516844ee37faf82ca1846dc
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-06 20:22:26 +0000

    2 args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    args = sys.argv[1:]
+    arg_count = len(args)
+
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg_count))
+
+    for i, arg in enumerate(args, 1):
+        print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    main()
