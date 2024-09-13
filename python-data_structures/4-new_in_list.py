648460af169bec6ab61a4bb7615079ee0bdb00da
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-13 11:52:40 +0000

    fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    new_list = my_list[:]
+        return my_list
+    new_list = my_list.copy()
     new_list[idx] = element
     return new_list
