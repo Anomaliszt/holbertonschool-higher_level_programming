a35ee22f97a45ffb2e7403740caa9acd7a70ba03
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-13 12:57:20 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,11 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    x = 0
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
     for i in range(len(roman_string)):
-        if i > 0 and rom[roman_string[i]] > rom[roman_string[i - 1]]:
-            x += rom[roman_string[i]] - (2 * rom[roman_string[i - 1]])
+        if i > 0 and dict[roman_string[i]] > dict[roman_string[i - 1]]:
+            num += dict[roman_string[i]] - 2 * dict[roman_string[i - 1]]
         else:
-            x += rom[roman_string[i]]
-    return x
-
-
+            num += dict[roman_string[i]]
+    return num
