0c80fe05a2000d205a3f3c5e4d0cde4859ea8919
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-13 11:54:35 +0000

    fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
+    if sentence == "":
+        return (0, None)
     tuple = (len(sentence), sentence[0])
     return tuple
