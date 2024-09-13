7a8b6f1c7429cf297bc7c86933464019378850e5
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-13 12:39:39 +0000

    fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-
     if len(my_list) == 0:
         return (None)
 
     value = my_list[0]
     for i in range(len(my_list)):
-         if my_list[i] > value:
-             value = my_list[i]
+        if my_list[i] > value:
+            value = my_list[i]
     return (value)
