80a4f44d31c6e07220c26683a6a85a3738f523dd
Saki <saki@LeHote.myguest.virtualboxorg>
2024-09-17 14:17:45 +0200

    letter fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         size: size of square
         """
         if not isinstance(size, int):
-            raise TypeError("size mut be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
