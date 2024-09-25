8f8dd7c0b827e1fd42ff5c5d7fbcb272c68d640c
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-25 09:07:31 +0000

    fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,33 +2,7 @@
 """ 3 classes, basegeometry, rectangle and square """
 
 
-class BaseGeometry:
-    """ class basegeometry"""
-    def integer_validator(self, name, value):
-        """ method that validates int for basegeometry"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ class rectangle which inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """ method that initializes rectangle """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """ method to use print function"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        """ method that returns area of rectangle"""
-        return self.__width * self.__height
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """ class square which inherits from rectangle"""
