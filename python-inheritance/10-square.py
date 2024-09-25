0e11a67086c870fc43f99fe00b7d5dc90df239f8
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-25 08:29:20 +0000

    fixed with super

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -35,6 +35,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """ method that initializes square"""
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
