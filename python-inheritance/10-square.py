078c8295030eac40a1ccc757b23f407461e1c917
Anomaliszt <xavier.sanchez44@orange.fr>
2024-09-25 07:41:16 +0000

    square area

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -20,9 +20,11 @@ class Rectangle(BaseGeometry):
 
 class Square(Rectangle):
     def __init__(self, size):
-        super().__init__()
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
         return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
