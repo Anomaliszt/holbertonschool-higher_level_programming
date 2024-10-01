e8129f36ff5be51cfaa4d5278bc59dd83d6a1fb1
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-01 10:33:41 +0000

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ and then save them to a file
 """
 import json
 import sys
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -12,3 +13,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 my_obj = []
+
+if os.path.exists(filename):
+    my_obj = load_from_json_file(filename)
+
+if len(sys.argv) > 1:
+    for i in sys.argv[1:]:
+        my_obj.append(i)
+
+save_to_json_file(my_obj, filename)
