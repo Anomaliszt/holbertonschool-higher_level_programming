23605401d850b2425af0fba716371dab8b8b4e21
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-02 14:36:25 +0000

    fixed trailing whitespace

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
         return []
     tri = [[1]]
     for x in range(1, n):
-        row =[1]
+        row = [1]
         for y in range(1, x):
             row.append(tri[x-1][y-1] + tri[x-1][y])
         row.append(1)
