13876be6309db34f4bfaf16c9ca403c460e725fe
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-13 18:58:31 +0200

    changed 404 message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -43,7 +43,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
