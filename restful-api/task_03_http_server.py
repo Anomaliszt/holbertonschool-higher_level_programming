1985a355c3f2721b6de17e309c48ce1dfcc8223f
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-13 18:52:24 +0200

    fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,13 +34,17 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {"version": "1.0", "description": "A simple API built with http.server"}
+            data = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
             self.wfile.write(json.dumps(data).encode('utf-8'))
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"404 Not Found")
+
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     httpd.serve_forever()
