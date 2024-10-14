31dfa5ca9cac8c8be6ba3db8c7665c64ad4c4a17
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-14 13:49:35 +0200

    moved verification back down

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -17,10 +17,6 @@ def home():
 users = {{"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}}
 
 
-if __name__ == "__main__":
-    app.run()
-    
-
 @app.route('/data')
 def current_users():
     """ Return the list of users """
@@ -69,3 +65,5 @@ def add_user():
     }), 201
 
 
+if __name__ == "__main__":
+    app.run()
