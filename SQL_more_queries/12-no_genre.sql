3581a00e417754bc97b31a5cbef7853ba431b87d
Anomaliszt <xavier.sanchez44@orange.fr>
2024-11-04 16:10:26 +0100

    fixed

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,14 @@
--- Desc: List the titles of all TV shows that do not have a genre.
-SELECT s.`title` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` WHERE g.`genre_id` IS NULL ORDER BY s.`title`;
+-- Desc: Find all TV shows that do not have a genre assigned to them.
+SELECT 
+    s.`title`, 
+    g.`genre_id` 
+FROM 
+    `tv_shows` AS s 
+LEFT JOIN 
+    `tv_show_genres` AS g 
+ON 
+    s.`id` = g.`show_id` 
+WHERE 
+    g.`genre_id` IS NULL 
+ORDER BY 
+    s.`title`;
